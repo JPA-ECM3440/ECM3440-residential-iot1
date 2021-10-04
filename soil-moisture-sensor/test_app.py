257ac9207ccf2c5f8b8d21ad9eff41c4e798e722
jdday <jamiedday@hotmail.co.uk>
2021-10-04 12:34:18 +0100

    Added spec to relay/adc/client magic mock definitions

--- soil-moisture-sensor/test_app.py
+++ soil-moisture-sensor/test_app.py
@@ -1,13 +1,17 @@
 from unittest.mock import MagicMock, patch, mock_open
 
+from azure.iot.device import IoTHubDeviceClient
+from counterfit_shims_grove.adc import ADC
+from counterfit_shims_grove.grove_relay import GroveRelay
+
 from app_class import SoilMoistureMonitor, connect, main
 
 
 @patch('app_class.MethodResponse')
 def test_relay_on(mock_method_response):
-    mock_device_client = MagicMock()
-    mock_adc = MagicMock()
-    mock_relay = MagicMock()
+    mock_device_client = MagicMock(spec=IoTHubDeviceClient)
+    mock_adc = MagicMock(spec=ADC)
+    mock_relay = MagicMock(spec=GroveRelay)
     mock_request = MagicMock()
 
     mock_request.name = 'relay_on'
@@ -24,9 +28,9 @@ def test_relay_on(mock_method_response):
 
 @patch('app_class.MethodResponse')
 def test_relay_off(mock_method_response):
-    mock_device_client = MagicMock()
-    mock_adc = MagicMock()
-    mock_relay = MagicMock()
+    mock_device_client = MagicMock(spec=IoTHubDeviceClient)
+    mock_adc = MagicMock(spec=ADC)
+    mock_relay = MagicMock(spec=GroveRelay)
     mock_request = MagicMock()
 
     mock_request.name = 'relay_off'
@@ -46,9 +50,9 @@ def test_relay_off(mock_method_response):
 @patch('app_class.json')
 @patch('app_class.Message')
 def test_get_reading(mock_message, mock_json):
-    mock_device_client = MagicMock()
-    mock_adc = MagicMock()
-    mock_relay = MagicMock()
+    mock_device_client = MagicMock(spec=IoTHubDeviceClient)
+    mock_adc = MagicMock(spec=ADC)
+    mock_relay = MagicMock(spec=GroveRelay)
     mock_soil_moisture_monitor = SoilMoistureMonitor(adc=mock_adc, device_client=mock_device_client, relay=mock_relay)
     mock_soil_moisture_monitor.get_reading()
     mock_adc.read.assert_called_once_with(0)
@@ -58,9 +62,9 @@ def test_get_reading(mock_message, mock_json):
 
 @patch('app_class.IoTHubDeviceClient')
 def test_connect(mock_client):
-    mock_device_client = MagicMock()
-    mock_adc = MagicMock()
-    mock_relay = MagicMock()
+    mock_device_client = MagicMock(spec=IoTHubDeviceClient)
+    mock_adc = MagicMock(spec=ADC)
+    mock_relay = MagicMock(spec=GroveRelay)
     mock_client.create_from_connection_string.return_value = mock_device_client
     return_value = connect('test_value')
     mock_client.create_from_connection_string.assert_called_once_with('test_value')
@@ -76,7 +80,7 @@ def test_connect(mock_client):
 @patch('app_class.ADC')
 def test_main(mock_adc, mock_grove_relay, mock_soil_moisture_monitor, mock_take_readings, mock_connect, mock_getenv):
     mock_getenv.return_value = 'test_connection_string'
-    mock_device_client = MagicMock()
+    mock_device_client = MagicMock(spec=IoTHubDeviceClient)
     mock_connect.return_value = mock_device_client
     main()
     mock_adc.assert_called_once()
